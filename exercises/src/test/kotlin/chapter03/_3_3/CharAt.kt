46555a7db4dfb7c8e6a366ac076da6768e088593
young-hwang <ykcul02@gmail.com>
2025-04-10 00:27:12 +0900

    feat: exercise 3.3

---
+++ exercises/src/test/kotlin/chapter03/_3_3/CharAt.kt
@@ -0,0 +1,15 @@
+package chapter03._3_3
+
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+class CharAt {
+    fun buildCharAtPos(s: String): (Int) -> Char = { i -> s.get(i) }
+
+    @Test
+    fun `Character retrieval by index`() {
+        val myCharAtPos = buildCharAtPos("Kotlin")
+        expectThat(myCharAtPos(0)).isEqualTo('K')
+    }
+}
